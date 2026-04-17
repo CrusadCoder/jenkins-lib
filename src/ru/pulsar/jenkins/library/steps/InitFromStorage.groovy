44d88fa52b375861ec88697dfcfe940630c7f18d
Dmitrii <dmitri@example.com>
2026-04-17 14:59:40 +0300

    Fix_15

--- src/ru/pulsar/jenkins/library/steps/InitFromStorage.groovy
+++ src/ru/pulsar/jenkins/library/steps/InitFromStorage.groovy
@@ -54,12 +54,10 @@ class InitFromStorage implements Serializable {
         ]) {
             // ===== DEBUG =====
             def env = ContextRegistry.getContext().getCurrentBuild().getEnvironment()
-            
+
             Logger.println('===== STORAGE DEBUG =====')
             Logger.println("USER credential ID: ${storageCredentials}")
             Logger.println("PATH credential ID: ${storagePath}")
-            Logger.println('USER length = ' + env.RUNNER_STORAGE_USER?.length())
-            Logger.println('PATH = ' + env.RUNNER_STORAGE_NAME)
 
             Logger.println('Выполнение загрузки конфигурации из хранилища')
             String vrunnerPath = VRunner.getVRunnerPath()
