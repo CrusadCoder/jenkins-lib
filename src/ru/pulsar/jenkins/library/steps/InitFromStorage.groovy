93d0612ffea1d81206c6b60f2a6c81e01a7325a3
Dmitrii <dmitri@example.com>
2026-04-17 13:57:42 +0300

    Fix_10

--- src/ru/pulsar/jenkins/library/steps/InitFromStorage.groovy
+++ src/ru/pulsar/jenkins/library/steps/InitFromStorage.groovy
@@ -51,12 +51,16 @@ class InitFromStorage implements Serializable {
                 storagePath,
                 'RUNNER_STORAGE_NAME'
             )
-        ])
-        {
-            echo "USER=${env.RUNNER_STORAGE_USER}"
-            echo "STORAGE=${env.RUNNER_STORAGE_NAME}"
-        }
-        {
+        ]) {
+            // ===== DEBUG =====
+            Logger.println('===== STORAGE DEBUG =====')
+            Logger.println("Credentials ID USER: ${storageCredentials}")
+            Logger.println("Credentials ID PATH: ${storagePath}")
+
+            Logger.println("ENV USER: ${env.RUNNER_STORAGE_USER}")
+            Logger.println("ENV PASS: ${env.RUNNER_STORAGE_PWD != null ? 'SET' : 'NULL'}")
+            Logger.println("ENV STORAGE PATH: ${env.RUNNER_STORAGE_NAME}")
+            
             Logger.println('Выполнение загрузки конфигурации из хранилища')
             String vrunnerPath = VRunner.getVRunnerPath()
             def command = "$vrunnerPath update-dev --storage $storageVersionParameter --ibconnection \"/F./build/ib\""
